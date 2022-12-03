80ffa3bdcb9feda800b46f693895a5ef471141ac
kazehiki03 <kazehiki30.dev@gmail.com>
2022-12-03 16:21:53 +0900

    [add] ディレクトリ細分化とルーティング実装

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles/index.css';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import App from './pages/App';
+import About from "./pages/About";
+import Howto from "./pages/Howto";
+import Todo from "./pages/Todo";
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    hello
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="about" element={<About />} />
+        <Route path="howto" element={<Howto />} />
+        <Route path="todo" element={<Todo />} />
+        <Route
+        path="*"
+        element={
+          <main>
+            <p>存在しないページです</p>
+          </main>
+        }/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
